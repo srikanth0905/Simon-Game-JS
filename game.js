82e90e26db46bde4ce7bbdab485970cb501a5871
Greymat9er <greymat9er@gmail.com>
2023-04-21 17:30:56 +0530

    generating random colors

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-alert("hello");
+var gamePattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+var randomChosenColor = buttonColors[nextSequence()];
+
+gamePattern.push(randomChosenColor);
+console.log(gamePattern);
+
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber;
+}
+
