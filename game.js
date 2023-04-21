66c00008d3c5c33738e427d6be362de0309a5865
Greymat9er <greymat9er@gmail.com>
2023-04-21 18:03:53 +0530

    play sound on user clicks

--- game.js
+++ game.js
@@ -1,14 +1,37 @@
 var gamePattern = [];
-
+var userClickedPattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
-var randomChosenColor = buttonColors[nextSequence()];
-
-gamePattern.push(randomChosenColor);
-console.log(gamePattern);
 
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
+    var randomChosenColor = buttonColors[randomNumber];
+    
+    console.log(randomChosenColor);
+
+    gamePattern.push(randomChosenColor);
+
+    // animate the button
+    $("#" + randomChosenColor).fadeOut(150).fadeIn(150);
+
+    playSound(randomChosenColor)
+}
+
+
+$(".btn").click(function () {
+    var userChosenColor = $(this).attr('id');
+    
+    // adding user pattern to array
+    userClickedPattern.push(userChosenColor);
+    console.log(userChosenColor);
+
+    playSound(userChosenColor);
+
+});
+
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();        
 }
 
